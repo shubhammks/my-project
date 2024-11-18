438583b03521a91a299d7eaa8c5d58a8c8a66a29
Shubham Shinde <shuhammks22@gmail.com>
2024-11-18 20:57:43 +0530

    final1.2

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let divs = document.querySelectorAll('.card');
 divs.forEach((div) => {
     let dis = div.querySelector('.descri');
+    // let cr=div.querySelector('.cross');
     div.addEventListener('click', () => {
-        if (dis.style.display === "none") {
-            dis.style.display = "block";
-        }
-        else {
-            dis.style.display = "none";
-        }
+        dis.classList.toggle("block");
+        // dis.style.display="block";  
     })
+        // console.log(cr);
+        // cr.addEventListener('click',()=>{
+        //     console.log('ok');
+        //     dis.style.display="none";  
+        // })
+    
 })
